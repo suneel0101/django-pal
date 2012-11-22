a75464dd606fe55e9ec8604bef082a935ed2a46a
suneel0101 <suneel0101@gmail.com>
2012-11-22 16:59:07 -0500

    command v2: now a class

--- create.py
+++ create.py
@@ -7,71 +7,87 @@ def run(*args, **kwargs):
     call(*args, shell=True, **kwargs)
 
 
-def main():
-    usage = "usage: %prog --path=PATH_TO_PROJ --name=PROJ_NAME"
-    parser = OptionParser(usage)
-    parser.add_option("-p", "--path", dest="path",
-                      action="store", type="string",
-                      help="project path")
-    parser.add_option("-n", "--name", dest="name",
-                      action="store", type="string",
-                      help="project name")
-    (options, args) = parser.parse_args()
+class Command(object):
+    def main(self):
 
-    template = 'django-foundation'
-    name = options.name
-    destination = options.path
+        usage = "usage: %prog --path=PATH_TO_PROJ --name=PROJ_NAME"
+        parser = OptionParser(usage)
+        parser.add_option("-p", "--path", dest="path",
+                          action="store", type="string",
+                          help="project path")
+        parser.add_option("-n", "--name", dest="name",
+                          action="store", type="string",
+                          help="project name")
+        parser.add_option("-t", "--template", dest="template",
+                          action="store", type="string",
+                          help="project template path or URL")
 
-    # If destination is entered without a trailing slash
-    # Append a trailing slash
-    if destination[-1] != '/':
-        destination = "{}/".format(destination)
+        (options, args) = parser.parse_args()
 
-    prepare()
-    create_project(template, name, destination)
-    # Change directory into newly created project directory
-    os.chdir(destination)
-    deploy()
+        self.template = options.template or 'django-foundation'
+        self.name = options.name
+        self.destination = options.path
 
+        # If destination is entered without a trailing slash
+        # Append a trailing slash
+        if self.destination[-1] != '/':
+            self.destination = "{}/".format(self.destination)
+        self.full_destination = "{}{}".format(self.destination, self.name)
 
-def prepare():
-    """
-    Grab django project template
-    Create a virtual environment
-    Activate virtual environment
-    Install Django
-    """
-    run('git clone git://github.com/suneel0101/django-foundation.git')
-    run('virtualenv venv --distribute')
-    run('source venv/bin/activate')
-    run('pip install django')
+        self.prepare()
+        self.create_project()
+        # Change directory into newly created project directory
+        import pdb; pdb.set_trace()
+        os.chdir(self.full_destination)
+        self.deploy()
 
 
-def create_project(template, name, destination):
-    """
-    Create Django project according to `template` at `destination`
-    with project name `name`
-    Create active.py, which will be git ignored
-    """
-    run('django-admin.py startproject --template={} {} {}'.format(
-            template, name, destination))
-    full_destination = "{}{}".format(destination, name)
-    run('touch {}/settings/active.py'.format(full_destination, name))
+    def prepare(self):
+        """
+        Grab django project template
+        Create a virtual environment
+        Activate virtual environment
+        Install Django
+        """
+        run('git clone git://github.com/suneel0101/django-foundation.git')
+        run('virtualenv venv --distribute')
+        run('source venv/bin/activate')
+        run('pip install django')
 
 
-def deploy():
-    """
-    Initialize a git repo.
-    Create a Heroku app.
-    Push to Heroku.
-    Scale the web process.
-    """
-    run('git init')
-    run('heroku create')
-    run("git add . && git commit -m 'pushing to heroku'")
-    run("git push heroku master")
-    run("heroku ps:scale web=1")
-    run("heroku open")
+    def create_project(self):
+        """
+        Create Django project according to `template` at `destination`
+        with project name `name`
+        Create active.py, which will be git ignored
+        """
+
+
+        original_cwd = os.getcwd()
+        os.chdir(self.destination)
+        run('mkdir {}'.format(self.name))
+        os.chdir(original_cwd)
+        run(u'django-admin.py startproject --template={} {} {}'.format(
+                self.template, self.name, self.full_destination))
+
+        run('touch {}/settings/active.py'.format(self.full_destination, self.name))
+
+
+    def deploy(self):
+        """
+        Initialize a git repo.
+        Create a Heroku app.
+        Push to Heroku.
+        Scale the web process.
+        """
+        run('git init')
+        run('heroku create')
+        run("git add . && git commit -m 'pushing to heroku'")
+        run("git push heroku master")
+        run("heroku ps:scale web=1")
+        run("heroku run python manage.py syncdb")
+        run("heroku run python manage.py migrate")
+        run("heroku open")
 
 if __name__ == "__main__":
-    main()
+    Command().main()
